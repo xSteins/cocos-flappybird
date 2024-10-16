2f551602321d71a6f69ba9f0dc0dee543347f84e
Ralvin Adrian <itsmesteins@gmail.com>
2024-10-16 08:18:27 +0700

    update bird init position to not jump

--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -18,26 +18,40 @@ export class Player extends Component {
   @property({ type: CCInteger })
   public gravityValue: number = 300
   @property({ type: CCInteger })
-  public playerMoveHeight: number = 500
+  public playerMoveHeight: number = 340
 
   @property({ type: Node })
   public restartMenu: Node
 
   start() {
     // input handling langsung mengarah ke objek bird
-    input.on(Input.EventType.TOUCH_START, this.moveBirdPosition, this)
+    input.on(Input.EventType.TOUCH_START, this.onTouchStart, this)
+  }
 
+  onTouchStart() {
+    this.moveBirdPosition()
   }
 
   moveBirdPosition() {
-    this.playerMoveHeight = 340
+    // untuk gerakin playernya
+    this.playerMoveHeight = 340;
     // tambahan sfx setiap input diterima
     this.getComponent(AudioSource).playOneShot(this.getComponent(AudioSource).clip);
   }
 
+  generateUpdatePosition(deltaTime: number) {
+    // ini untuk diupdate incremental berdasarkan waktu (todo)
+    // saat ini dipake untuk checking supaya birdnya tidak loncat keatas dan langsung gameover
+    return deltaTime * this.playerMoveHeight;
+  }
 
   update(deltaTime: number) {
-    this.node.translate(new Vec3(0, this.playerMoveHeight * deltaTime, 0))
+    // console.log(this.node.getPosition())
+    // console.log("update ", this.generateUpdatePosition(deltaTime))
+    // console.log("gravity ", this.playerMoveHeight - this.gravityValue * deltaTime)
+    if (this.generateUpdatePosition(deltaTime) < 100) {
+      this.node.translate(new Vec3(0, this.generateUpdatePosition(deltaTime), 0))
+    }
     this.playerMoveHeight -= this.gravityValue * deltaTime
   }
 }
