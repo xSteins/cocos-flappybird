0019bcf589e6a82cad6c178d7a26539f61d922d3
Ralvin Adrian <itsmesteins@gmail.com>
2024-10-27 09:25:03 +0700

    revert generateXPosition karena harus difix saja

--- assets/Scripts/Obstacle.ts
+++ assets/Scripts/Obstacle.ts
@@ -1,50 +1,49 @@
-  import { _decorator, CCFloat, CCInteger, Component, Node, Vec3 } from 'cc'
-  import { GameController } from './GameController'
-  const { ccclass, property } = _decorator
+import { _decorator, CCFloat, Component, Vec3 } from 'cc'
+import { GameController } from './GameController'
+const { ccclass, property } = _decorator
 
-  @ccclass('Obstacle')
-  export class Obstacle extends Component {
-    //buat edit kecepatan,batas geser gambar awal, sama posisi gambar baru
-    public gameCtrlSpeed = new GameController()
-    public gameSpeed: number
-    // buat batas generate posisi x-axis dari vector
-    @property({ type: CCFloat })
-    public maxMovePos: number = -376.331
-    @property({ type: CCFloat })
-    public newVecPos: number = 385.05
+@ccclass('Obstacle')
+export class Obstacle extends Component {
+  public gameCtrlSpeed = new GameController()
+  public gameSpeed: number
 
-    // buat batas randomize posisi y-axis vector
-    private maxHeightPos: number = 180
-    public minHeightPos: number = -270
+  @property({ type: CCFloat })
+  public maxMovePos: number = -476.331 // +100 supaya edge case special obstale gak hilang sblum out of view
 
-    // generate
-    generateYPosition(): number {
-      // todo : fix min max ngebug malah ngambil dari x-axis??
-      // console.log('max',this.maxHeightPos)
-      // console.log('min',this.minHeightPos)
-      let pos =
-        Math.floor(Math.random() * (this.maxHeightPos - this.minHeightPos)) +
-        this.minHeightPos
-      return pos
-      // return Math.floor(Math.random() * (this.maxHeightPos - this.minHeightPos)) + this.minHeightPos;
-    }
-    resetPipe() {
-      // kalo pipa spesial, override this.newVecPosnya
-      let generateXPosition = (this.node.name !== 'Obstacle') && (this.node.name !== 'Obstacle2') ? this.newVecPos + 200 : this.newVecPos;
-      this.node.setPosition(
-        new Vec3(generateXPosition, this.generateYPosition(), 0)
-      );
-      this.node['hasPassed'] = false;
-    }
+  @property({ type: CCFloat })
+  public newVecPos: number = 385.05
 
-    update(deltaTime: number) {
-      this.gameSpeed = this.gameCtrlSpeed.speed // ambil value global dari controller
-      // console.log("CURR POS " + this.node.position.x);
-      this.node.translate(new Vec3(-this.gameSpeed * deltaTime, 0, 0))
+  private maxHeightPos: number = 180
+  public minHeightPos: number = -270
 
-      // kalo sudah mendekati ujung elemen node, harus regenerate vector baru
-      if (this.node.position.x <= this.maxMovePos) {
-        this.resetPipe()
-      }
+  // generate
+  generateYPosition(): number {
+    // todo : fix min max ngebug malah ngambil dari x-axis??
+    // console.log('max',this.maxHeightPos)
+    // console.log('min',this.minHeightPos)
+    let pos =
+      Math.floor(Math.random() * (this.maxHeightPos - this.minHeightPos)) +
+      this.minHeightPos
+    return pos
+    // return Math.floor(Math.random() * (this.maxHeightPos - this.minHeightPos)) + this.minHeightPos;
+  }
+
+  resetPipe() {
+    // let generateXPosition =
+    //   this.node.name !== 'Obstacle' && this.node.name !== 'Obstacle2'
+    //     ? this.newVecPos + 200 : this.newVecPos // ini buat cek kalo dia special obstacle, maka reset positionnya +200 dari node obstacle biasa
+    this.node.setPosition(
+      new Vec3(this.newVecPos, this.generateYPosition(), 0)
+    )
+    this.node['hasPassed'] = false
+  }
+
+  update(deltaTime: number) {
+    this.gameSpeed = this.gameCtrlSpeed.speed
+    this.node.translate(new Vec3(-this.gameSpeed * deltaTime, 0, 0))
+
+    if (this.node.position.x <= this.maxMovePos) {
+      this.resetPipe()
     }
   }
+}
